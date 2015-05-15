03c42193491a44284e9417d32aff56a1fdf16563
Anton Trofimenko (Mariupol) <a.trofimenko@mobidev.biz>
2015-05-15 20:04:09 +0300

    Update project for publishing.

--- example/index.js
+++ example/index.js
@@ -1,22 +1,20 @@
-    var $div = document.querySelector('#pointer');
-    var $console = document.querySelector('#console');
-    var pointer = new PointerTracker($div);
-
-    for(var eventName in pointer.EVENTS){
-        console.log(pointer.EVENTS[eventName]);
-        $div.addEventListener(pointer.EVENTS[eventName], function (event) {
-            var $eventElement = document.createElement('div');
-            $eventElement.innerHTML = getTime()+': ' + event.type;
-            $console.insertBefore($eventElement, $console.firstChild);
-        }, false);
-    }
-    function getTime() {
-        var currentdate = new Date();
-        return ((currentdate.getHours()<10) ? "0"+currentdate.getHours() : currentdate.getHours()) + ":"
-            + ((currentdate.getMinutes()<10) ? "0" + currentdate.getMinutes() : currentdate.getMinutes())+":"
-            + ((currentdate.getSeconds()<10) ? "0"+currentdate.getSeconds() : currentdate.getSeconds());
-    }
-
-    function onChange() {
-        pointer.setMoveHoverState(document.getElementById('checkbox').checked);
-    }
+var $div = document.querySelector('#pointer');
+var $console = document.querySelector('#console');
+var pointer = new PointerTracker($div);
+for (var eventName in pointer.EVENTS) {
+	console.log(pointer.EVENTS[eventName]);
+	$div.addEventListener(pointer.EVENTS[eventName], function (event) {
+		var $eventElement = document.createElement('div');
+		$eventElement.innerHTML = getTime() + ': ' + event.type;
+		$console.insertBefore($eventElement, $console.firstChild);
+	}, false);
+}
+function getTime() {
+	var currentdate = new Date();
+	return ((currentdate.getHours() < 10) ? "0" + currentdate.getHours() : currentdate.getHours()) + ":"
+		+ ((currentdate.getMinutes() < 10) ? "0" + currentdate.getMinutes() : currentdate.getMinutes()) + ":"
+		+ ((currentdate.getSeconds() < 10) ? "0" + currentdate.getSeconds() : currentdate.getSeconds());
+}
+function onChange() {
+	pointer.setMoveHoverState(document.getElementById('checkbox').checked);
+}
--- minimizer.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-uglifyjs pointer.js --compress --mangle --screw-ie8 --comments --lint --source-map pointer.min.js.map --output pointer.min.js
