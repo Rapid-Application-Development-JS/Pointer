abf7217142f96a123c7c8cced2ddad25d35bd8ce
Prokofeyv Roman <r.prokofeyv@mobidev.biz>
2015-07-23 11:32:57 +0300

    clean example

--- example/index.js
+++ example/index.js
@@ -1,14 +1,15 @@
 var $div = document.querySelector('#pointer');
 var $console = document.querySelector('#console');
 var pointer = new PointerTracker($div);
+
 for (var eventName in pointer.EVENTS) {
-	console.log(pointer.EVENTS[eventName]);
 	$div.addEventListener(pointer.EVENTS[eventName], function (event) {
 		var $eventElement = document.createElement('div');
 		$eventElement.innerHTML = getTime() + '; pointerId: ' + event.pointerId+ '; type: '+ event.type+ '; x:y '+event.x+':'+event.y;
 		$console.insertBefore($eventElement, $console.firstChild);
 	}, false);
 }
+
 function getTime() {
 	var currentdate = new Date();
 	return ((currentdate.getHours() < 10) ? "0" + currentdate.getHours() : currentdate.getHours()) + ":"
